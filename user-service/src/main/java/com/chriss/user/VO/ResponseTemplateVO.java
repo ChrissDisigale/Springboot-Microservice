0c45bcc40104427f999993adbef17a27f6d88c01
unknown <chrispro.genius@gmail.com>
2022-03-14 22:19:24 +0300

    first commit

---
+++ user-service/src/main/java/com/chriss/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,16 @@
+package com.chriss.user.VO;
+
+import com.chriss.user.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
